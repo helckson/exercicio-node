539abad46ccdc8733e55edc60bd8ef6b5f8519c4
helckson <helckson.ishibashi@hotmail.com>
2023-09-23 18:18:14 -0300

    implementação dos métodos PUT e DELETE

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -30,4 +30,56 @@ export class UserController {
             return res.status(500).json({ message: 'Internal Server Error'})
         }
     }
+
+    async listby(req: Request, res: Response) {
+        const { idUser } = req.params
+
+        const user = await userRepository.findOneBy({ id: Number(idUser)})
+
+        if(user) {
+            res.json(user)
+        }else {
+            res.status(500).json('Usuario não encontrado')
+        }
+    }
+
+    async update(req: Request, res: Response) {
+     try {
+        const { idUser } = req.params
+        const userData = req.body
+
+        const user = await userRepository.findOneBy({id: Number(idUser)})
+
+        if(!user) {
+            return res.status(400).json('Usuario não encontrado')
+        }
+
+        await userRepository.update(user, userData);
+        
+        return res.json("Usuario atualizado com sucesso")
+     } catch (error) {
+        console.error('Erro ao atualizar usuário:', error);
+        return res.status(500).json('Erro interno do servidor');
+     }  
+    }
+
+    async delete(req: Request, res: Response) {
+        try {
+            const { idUser } = req.params
+            const user = await userRepository.findOneBy({id: Number(idUser)})
+
+            if(!user) {
+                return res.status(400).json('Usuario não encontrado')
+            }
+            
+            await userRepository.delete(idUser);
+
+            return res.status(200).json('Usuario deletado com sucesso');
+
+        } catch (error) {
+            console.error('Erro ao atualizar usuário:', error);
+            return res.status(500).json('Erro interno do servidor');
+        }
+
+    }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -5,5 +5,8 @@ const routes = Router();
 
 routes.post('/user', new UserController().create)
 routes.get('/user', new UserController().list)
+routes.get('/user/:idUser', new UserController().listby)
+routes.put('/user/:idUser', new UserController().update)
+routes.delete('/user/:idUser', new UserController().delete)
 
 export default routes
